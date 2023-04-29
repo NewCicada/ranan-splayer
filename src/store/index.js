8cecb7dae9af5511675153edcb0d8b8b766dcbaa
NewCicada <1547598968@qq.com>
2023-04-29 21:49:56 +0800

    feat: 增添store状态管理

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
-import useMusicDataStore from "./musicData"
+import {
+  defineStore
+} from 'pinia'
 import useSettingDataStore from "./settingData"
+import useMusicDataStore from "./musicData"
+import useUserDataStore from "./userData"
 
 export const settingStore = () => useSettingDataStore()
-
 export const musicStore = () => useMusicDataStore()
+export const userStore = () => useUserDataStore()
