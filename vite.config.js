f45bb28001c25f3d8d73189a0848cf6cc06e5295
NewCicada <1547598968@qq.com>
2023-04-29 21:37:22 +0800

    chore:更新vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueRouter from 'unplugin-vue-router/vite'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -9,51 +9,59 @@ import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    // ⚠️ Vue must be placed after VueRouter()
-    vue(),
-    'vue',
-    {
-      'naive-ui': [
-        'useDialog',
-        'useMessage',
-        'useNotification',
-        'useLoadingBar',
-      ],
-    },
-    VueRouter({
-      /* options */
-      resolvers: [NaiveUiResolver()],
-    }),
-    AutoImport({
-      imports: ['vue', 'vue-router'],
-      dts: './auto-import.d.ts', // or a custom path
-      eslintrc: {
-        enabled: true, // <-- this
+export default ({ mode }) =>
+  defineConfig({
+    plugins: [
+      // ⚠️ Vue must be placed after VueRouter()
+      vue(),
+      'vue',
+      {
+        'naive-ui': [
+          'useDialog',
+          'useMessage',
+          'useNotification',
+          'useLoadingBar',
+        ],
       },
-    }),
-    Components({
-      /*option*/
-      resolvers: [NaiveUiResolver()]
-    }),
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "src/style/index.scss";'
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      VueRouter({
+        /* options */
+        resolvers: [NaiveUiResolver()],
+      }),
+      AutoImport({
+        imports: ['vue', 'vue-router'],
+        dts: './auto-import.d.ts', // or a custom path
+        eslintrc: {
+          enabled: true, // <-- this
+        },
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()],
+      }),
+    ],
+    server: {
+      port: 8888,
+      open: true,
+      http: true,
+      ssr: false,
+      // 设置代理
+      proxy: {
+        '/api': {
+          target: loadEnv(mode, process.cwd()).VITE_MUSIC_API,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
     },
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src',
-        import.meta.url))
-    }
-  }
-})
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "src/style/index.scss";',
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  })
