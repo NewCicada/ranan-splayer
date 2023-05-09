4ade2fa43e65c46f6a956226a60217c2a49fa615
NewCicada <1547598968@qq.com>
2023-05-09 22:12:27 +0800

    feat: 歌曲评论

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,6 +1,6 @@
 import HomeView from '@/views/Home/HomeView.vue'
 import SearchView from '@/views/Search/index.vue'
-
+import comment from '@/views/Comment/index.vue'
 const routes = [
   {
     path: '/',
@@ -9,37 +9,51 @@ const routes = [
       title: '首页',
     },
     component: HomeView,
-  }, 
+  },
   {
     path: '/search',
     name: 'search',
     meta: {
-        title: '搜索',
+      title: '搜索',
     },
     component: SearchView,
-        redirect: '/search/songs',
-        children: [{
-            path: '/search/songs',
-            name: 's-songs',
-            component: () => import('@/views/Search/songs.vue'),
-        },{
-          path:'/search/artists',
-          name:'s-artists',
-          component:() => import('@/views/Search/artists.vue')
-        },{
-          path:'/search/albums',
-          name:'s-albums',
-          component:() => import('@/views/Search/albums.vue')
-        },{
-          path:'/search/videos',
-          name:'s-videos',
-          component:() => import('@/views/Search/videos.vue')
-        },{
-          path:'/search/playlists',
-          name:'s-playlists',
-          component:() => import('@/views/Search/playlists.vue')
-        }]
-  }
+    redirect: '/search/songs',
+    children: [
+      {
+        path: '/search/songs',
+        name: 's-songs',
+        component: () => import('@/views/Search/songs.vue'),
+      },
+      {
+        path: '/search/artists',
+        name: 's-artists',
+        component: () => import('@/views/Search/artists.vue'),
+      },
+      {
+        path: '/search/albums',
+        name: 's-albums',
+        component: () => import('@/views/Search/albums.vue'),
+      },
+      {
+        path: '/search/videos',
+        name: 's-videos',
+        component: () => import('@/views/Search/videos.vue'),
+      },
+      {
+        path: '/search/playlists',
+        name: 's-playlists',
+        component: () => import('@/views/Search/playlists.vue'),
+      },
+    ],
+  },
+  {
+    path: '/comment',
+    name: 'comment',
+    meta: {
+      title: '歌曲评论',
+    },
+    component: comment,
+  },
 ]
 
 export default routes
