0458aa931472277931cb9b03e4feefa2d3f90c40
NewCicada <1547598968@qq.com>
2023-05-22 16:55:59 +0800

    refactor: 重构专辑页面

--- src/views/Song/SongView.vue
+++ src/views/Song/SongView.vue
@@ -2,43 +2,74 @@
   <div class="song" v-if="musicDetail">
     <div class="detail">
       <div class="pic">
-        <n-avatar class="coverImg" :src="musicDetail.al.picUrl
-          ? musicDetail.al.picUrl.replace(/^http:/, 'https:') +
-          '?param=500y500'
-          : null
-          " fallback-src="/images/pic/default.png" />
+        <n-avatar
+          class="coverImg"
+          :src="
+            musicDetail.al.picUrl
+              ? musicDetail.al.picUrl.replace(/^http:/, 'https:') +
+                '?param=500y500'
+              : null
+          "
+          fallback-src="/images/pic/default.png"
+        />
       </div>
       <div class="right">
         <div class="intr">
           <n-text class="name text-hidden" v-html="musicDetail.name" />
-          <n-text depth="3" class="alia" v-if="musicDetail.alia[0]" v-html="musicDetail.alia[0]" />
+          <n-text
+            depth="3"
+            class="alia"
+            v-if="musicDetail.alia[0]"
+            v-html="musicDetail.alia[0]"
+          />
           <div class="all-artist">
             <n-text class="tip" depth="3">歌手：</n-text>
             <AllArtists v-if="musicDetail.ar" :artistsData="musicDetail.ar" />
           </div>
           <div class="album">
             <n-text class="tip" depth="3">专辑：</n-text>
-            <n-text class="text" v-html="musicDetail.al.name" @click="router.push(`/album?id=${musicDetail.al.id}`)" />
+            <n-text
+              class="text"
+              v-html="musicDetail.al.name"
+              @click="router.push(`/album?id=${musicDetail.al.id}`)"
+            />
           </div>
           <div class="time" v-if="musicDetail.publishTime">
             <n-text class="tip" depth="3">发行日期：</n-text>
-            <n-text class="text" v-html="getLongTime(musicDetail.publishTime)" />
+            <n-text
+              class="text"
+              v-html="getLongTime(musicDetail.publishTime)"
+            />
           </div>
         </div>
         <div class="button">
-          <n-button type="primary" strong secondary @click="addSong(musicDetail)">
+          <n-button
+            type="primary"
+            strong
+            secondary
+            @click="addSong(musicDetail)"
+          >
             <template #icon>
               <n-icon :component="PlayArrowRound" />
             </template>
             播放
           </n-button>
-          <n-button strong secondary @click="router.push(`/comment?id=${musicDetail.id}`)">
+          <n-button
+            strong
+            secondary
+            @click="router.push(`/comment?id=${musicDetail.id}`)"
+          >
             <template #icon>
               <n-icon :component="MessageFilled" />
             </template>
             评论
           </n-button>
-          <n-button strong secondary v-if="musicDetail.mv" @click="router.push(`/video?id=${musicDetail.mv}`)">
+          <n-button
+            strong
+            secondary
+            v-if="musicDetail.mv"
+            @click="router.push(`/video?id=${musicDetail.mv}`)"
+          >
             <template #icon>
               <n-icon :component="VideocamRound" />
             </template>
@@ -55,12 +86,12 @@
   </div>
 </template>
 
- 
 <script setup>
 import { getMusicDetail, getSimiPlayList } from "@/api";
 import { musicStore } from "@/store/index";
 import { getSongTime, getLongTime } from "@/utils/timeTools.js";
-import { formatNumber } from "@/utils/timeTools.js"
+import { PlayArrowRound, MessageFilled, VideocamRound } from "@vicons/material";
+import { formatNumber } from "@/utils/timeTools.js";
 
 const router = useRouter();
 const music = musicStore();
@@ -134,21 +165,18 @@ watch(
   }
 );
 </script>
- 
+
 <style lang="scss" scoped>
 .song {
   .detail {
     width: 80vw;
     display: flex;
     flex-direction: row;
-
     @media (max-width: 768px) {
       flex-direction: column;
-
       .pic {
         margin-bottom: 20px;
       }
-
       .right {
         .intr {
           .name {
@@ -157,7 +185,6 @@ watch(
         }
       }
     }
-
     .pic {
       height: auto;
       display: flex;
@@ -168,58 +195,47 @@ watch(
       border-radius: 8px;
       overflow: hidden;
       margin-right: 40px;
-
       .n-avatar {
         width: 100%;
         height: inherit;
       }
     }
-
     .right {
       flex: 1;
       display: flex;
       flex-direction: column;
       justify-content: space-evenly;
-
       .intr {
         display: flex;
         flex-direction: column;
-
         .name {
           -webkit-line-clamp: 2;
           font-size: 40px;
           font-weight: bold;
         }
-
         .alia {
           font-size: 20px;
         }
-
         .all-artist {
           margin-top: 12px;
           display: flex;
           flex-direction: row;
           align-items: center;
         }
-
         .album {
           margin: 4px 0;
-
           .text {
             cursor: pointer;
             transition: all 0.3s;
-
             &:hover {
               color: $mainColor;
             }
           }
         }
       }
-
       .button {
         margin-top: 16px;
         display: flex;
-
         button {
           margin-right: 16px;
         }
