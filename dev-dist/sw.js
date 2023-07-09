a2523e8d2ac4c50f9eac7274b70c5de796d11ba8
NewCicada <1547598968@qq.com>
2023-07-09 16:43:50 +0800

    更新sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-badb0bcf'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1tiu9ncknv"
+    "revision": "0.go5el8mcroo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
